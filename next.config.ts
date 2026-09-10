e4c7e8a8022dbf16246278b28c1c1235e8121c39
kwadwoaddo-collab <kwadwoaddo@googlemail.com>
2026-09-10 12:42:49 +0100

    fix(sentry): disable build-time sourcemap upload in withSentryConfig for CI build stability

--- next.config.ts
+++ next.config.ts
@@ -67,4 +67,8 @@ export default withSentryConfig(nextConfig, {
   silent: true,
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+  sourcemaps: {
+    disable: true,
+  },
+  disableLogger: true,
 });
